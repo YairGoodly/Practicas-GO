e401d6e5387821ba368e700dfa09f1937d49d7de
“YairGoodly” <f160034@gmail.com>
2019-09-24 20:27:42 -0500

    practica5

--- variables.go
+++ variables.go
@@ -2,22 +2,19 @@ package main
 import "fmt"
 func main(){
    //Declaracion de variables.
-   //Cuo := 1500.0
+   Cuo := 1500.0
    //Nom,ApP,ApM,Eda,Cuo,Acr := "Brandon","Cuatecatl","Cuapa",21,1500.00,false
    //Declaracion Matriz
    Meses := [] string{"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"}
-
-   //Ciclo de impresion
-   //3ra forma
-   for i, v := range Meses{
-     fmt.Println("Mes", i , v)
-   }
-   //4ta forma
-   //variable donde se guarda el tamaño de la Matriz
    size := len(Meses)
-   i := 0
-   for i < size {
-     fmt.Println(Meses[i])
-     i++
-     }
+   
+  //For unicamente utlizado para dar formato al mostrar los datos
+  i := 0
+  for i < size{
+    Prc := Cuo * .05
+    Cuo =  Cuo + Prc
+    fmt.Println(Meses[i:i+1], "Nueva Cuota")
+    fmt.Printf("%6.2f\n",Cuo)
+    i++
+  }
 }
